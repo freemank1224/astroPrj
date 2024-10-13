f9b9ed7cd7f5040541556bd270639b8fc132fc3c
Your Name <you@example.com>
2024-10-13 11:06:59 +0800

    Modify title info

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://dysonfreeman.astro.app/", // replace this with your deployed domain
+  author: "Dyson Freeman",
   profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "Personal insights for AI and Education.",
+  title: "Dyson's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
